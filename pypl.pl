a7e4d3432df4f40e8e4528c2d06633a0bc2d1d47
Mohamed Al Mouiee <m.almouiee@student.unsw.edu.au>
2017-09-14 16:06:51 +1000

    Subset 0 and most of 1 done

--- pypl.pl
+++ pypl.pl
@@ -1,33 +1,68 @@
 #!/usr/bin/perl -w
 
-# Starting point for COMP[29]041 assignment 1
-# http://www.cse.unsw.edu.au/~cs2041/assignments/pypl
-# written by andrewt@unsw.edu.au September 2017
-
-while ($line = <>) {
-    if ($line =~ /^#!/ && $. == 1) {
-
-        # translate #! line
-
-        print "#!/usr/bin/perl -w\n";
-
-    } elsif ($line =~ /^\s*(#|$)/) {
-
-        # Blank & comment lines can be passed unchanged
-
-        print $line;
-
-    } elsif ($line =~ /^\s*print\("(.*)"\)$/) {
-
-        # Python's print outputs a new-line character by default
-        # so we need to add it explicitly to the Perl print statement
-
-        print "print \"$1\\n\";\n";
-
-    } else {
-
-        # Lines we can't translate are turned into comments
-
-        print "#$line\n";
-    }
+open F, '<',"$ARGV[0]" or die;
+$progName = $ARGV[0];
+$progName =~ s/\.py/\.pl/;
+@perlLines =();
+while($line = <F>) {
+	chomp $line;
+	# Checks for empty lines in .py file
+	if($line eq "") {
+		push(@perlLines,"\n");
+		next;
+	}
+	#Subset 0
+	if($line =~ /\/python[23]/) {
+		$line =~ s/\/python[23]/\/perl -w/;
+	} else{
+		if($line =~ /print\(.*\)/) {
+			$line =~ s/\(|\)//g;
+		}
+		#Subset 1: deals with Variables, Constants and Maths operations										
+		if($line =~ /^[\w]* *= *[\w]*/) {
+			print"went in\n";
+			($first, $second) = $line =~ /^([\w]*) *= *([\w]*)/;
+			print"$second\n";
+			$line =~ s/($1)/\$$first /;
+			$line =~ s/= *[A-Za-z]*/= \$$second/;
+			#Maps equations with +
+			@matchesAdd = $line =~ /\+ *[A-Za-z]/g;
+			foreach $matchAdd (@matchesAdd) {
+				$matchAdd =~ s/\+ *//;
+				$line =~ s/\+ *$matchAdd/\+ \$$matchAdd/m;
+			}
+			#Maps equations with -
+			@matchesSub = $line =~ /\- *[A-Za-z]/g;
+			foreach $matchSub (@matchesSub) {
+				$matchSub =~ s/\- *//;
+				$line =~ s/\- *$matchSub/\-\$$matchSub/m;
+			}
+			#Maps equations with /
+			@matchesDiv = $line =~ /\/ *[A-Za-z]/g;
+			foreach $matchDiv (@matchesDiv) {
+				$matchDiv =~ s/\/ *//;
+				$line =~ s/\/ *$matchDiv/\/\$$matchDiv/m;
+			}
+			#Maps equations with **/^
+			@matchesTimes = $line =~ /\* *[A-Za-z]/g;
+			foreach $matchTimes (@matchesTimes) {
+				$matchTimes =~ s/\* *//;
+				$line =~ s/\* *$matchTimes/\*\$$matchTimes/m;
+			}
+			@matchesPowers = $line =~ /\*\* *[A-Za-z]/g;
+			foreach $matchPowers (@matchesPowers) {
+				$matchPowers =~ s/\*\* *//;
+				$line =~ s/\*\* *$matchPowers/\*\*\$$matchPowers/m;
+			}
+		}
+		$line .= ";";
+	}
+	push(@perlLines,$line);
 }
+close F;
+open my $F, '>', "$progName" or die;
+foreach $Line (@perlLines) {
+	print $F "$Line" if $Line ne "\n";
+	print $F "\n";
+}
+close $F;
