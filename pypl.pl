d950ba0d76c70db453430a47495d0928e3558d1b
Mohamed Al Mouiee <m.almouiee@student.unsw.edu.au>
2017-09-18 10:17:19 +1000

    fixed if still need to fix ending } for sep=1

--- pypl.pl
+++ pypl.pl
@@ -6,7 +6,7 @@
 
 sub variableMapper {
     my ($Line) = @_;
-    ($first, $second) = $Line =~ /^\t*([\w]*) *= *\(*([\w]*\)*)/;
+    ($first, $second) = $Line =~ /^ *([\w]*) *= *\(*([\w]*\)*)/;
 	$Line =~ s/($1)/\$$first /;
 	# If there are parentheses (at most 1) that wrap the variables
 	$Line =~ s/= *[A-Za-z]+[\w]*/= \$$second/ if $Line =~ /= *([\w]*)/;;
@@ -39,11 +39,15 @@ sub oppMapper {
     my @matchArr = @{$_[0]};
     $Line2 = $_[1];
     $sign = $_[2];
+    print"matchArr is @matchArr\n";
+    print"line is $line\n";
+    print"sign is $sign\n";
     foreach $matchEle (@matchArr) {
 	    $matchEle =~ s/(\+|\-|\*|\/) *//;
 	    $matchEle =~ s/\(//;
 	    $matchEle =~ s/\)//;
-	    $Line2 =~ s/(\+|\-|\*|\/) *\($matchEle/$sign \(\$$matchEle/m if $Line2 =~ /(\+|\-|\*|\/) *\(*[A-Za-z]\)*/;
+	    print"matchEle is $matchEle\n";
+        $Line2 =~ s/(\+|\-|\*|\/) *\($matchEle/$sign \(\$$matchEle/m if $Line2 =~ /(\+|\-|\*|\/) *\(*[A-Za-z]\)*/;
 	    $Line2 =~ s/(\+|\-|\*|\/) *$matchEle/$sign \$$matchEle/m if $Line2 =~ /(\+|\-|\*|\/) *[A-Za-z]/;
     }
     return $Line2;
@@ -51,15 +55,21 @@ sub oppMapper {
 
 sub ifMapper {
     my ($Line) = @_;
-    (my $first, my $second) = $Line =~ /if(.*):(.*)?/;
+    $boolSepLine = 1 if !($Line =~ /: *[A-Za-z]/);
+    (my $first) = $Line =~ /if *(.*):/;
     $Line =~ s/($1)/\($first\)/;
-    $Line =~ s/:.*/ \{/;
-    push(@perlLines,$Line);
-    if($second =~ / *[A-Za-z]/) {
-    	$second =~ s/^/	/;
-    	print"NOPE\n";
-    	push(@perlLines,$second);
-    	push(@perlLines,"}");
+    if ($boolSepLine == 0) {
+        (my $second) = $Line =~ /:(.*)/;
+        $Line =~ s/:.*/ \{/;
+        push(@perlLines,$Line);
+        $second =~ s/^ *//;
+        $second = variableMapper($second);
+        $second =~ s/^/    /;
+        push(@perlLines,$second);
+        push(@perlLines,"}");
+    } else {
+        $Line =~ s/:.*/ \{/;
+        push(@perlLines,$Line);
     }
 }
 
@@ -79,43 +89,31 @@ open F, '<',"$ARGV[0]" or die;
 $progName = $ARGV[0];
 $progName =~ s/\.py/\.pl/;
 @perlLines =();
-$i=1;
+$boolIf = 0;
+$boolSepLine = 0;
 while($line = <F>) {
 	chomp $line;
-	#Check for tabs at the beginning of a line S
-	#SOURCE: https://stackoverflow.com/questions/3916852/how-can-i-count-the-amount-of-spaces-at-the-start-of-a-string-in-perl
-	$startSpaces = $line;
-	$startSpaces =~ /^(\t*)/;
-	$countSpaces = length( $1 );
-	print"num of spaces $countSpaces\n";
 	# Checks for empty lines in .py file
 	if($line eq "") {
 		push(@perlLines,"\n");
-		$i++;
 		next;
 	}
 	#Subset 0
 	if($line =~ /\/python[23]/) {
 		$line =~ s/\/python[23]/\/perl -w/;
 	} else{
-		if($line =~ /print\(".*"\)/) {
+		if($line =~ /print\(.*\)/) {
 			$line =~ s/\("?/ "/;
 			$line =~ s/"?\)/\\n"/;
 		}
-		if($line =~ /print\([A-Za-z].*\)/) {
-			($var) = $line =~ /print\(([A-Za-z].*)\)/;
-			$line =~ s/\(/ "/;
-			$line =~ s/\)/"/;
-			$line =~ s/($var)/\$$var/;
-		}
 		#Subset 1: deals with Variables, Constants and Maths operations										
-		if($line =~ /^\t*[\w]* *= *\(*[\w]*\)*/) {
+		if($line =~ /^ *[\w]* *= *\(*[\w]*\)*/) {
 		    $line = variableMapper($line);
 		}
 		#Subset 2: deals with simple if, while, for and logical statements	
 		if($line =~ /if .*:/) {
+		    $boolIf = 1;
 		    ifMapper($line);
-		    $i++;
 		    next;
 		}
 		#if($line =~ /while .*:/) {
@@ -124,7 +122,6 @@ while($line = <F>) {
 		$line .= ";";
 	}
 	push(@perlLines,$line);
-	$i++;
 }
 close F;
 open $F, '>', "$progName" or die;
